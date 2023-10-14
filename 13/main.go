d52dcd920bc2fdb2c106f9e573f346b1267c82bd
lealrenato <renatolealhiss@gmail.com>
2023-10-14 13:47:00 -0300

    metodos em structs

--- 13/main.go
+++ 13/main.go
@@ -1,22 +1,49 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 )
 
-// funçõesjhgjhgjhg
-func main() {
-	valor, err := sum(1, 50)
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println(valor)
+type Endereco struct {
+	logradouro string
+	numero     int
+	cidade     string
+	estado     string
 }
 
-func sum(a int, b int) (int, error) {
-	if a+b >= 50 {
-		return 400, errors.New("a soma é maior que 50 ")
+type Cliente struct {
+	nome  string
+	idade int
+	ativo bool
+	Endereco
+}
+
+func (c Cliente) Desativar() {
+	c.ativo = false
+	fmt.Printf("cliente %s foi desativado \n", c.nome)
+	fmt.Printf("%t", c.ativo)
+}
+
+// funçõesjhgjhgjhg
+func main() {
+
+	renato := Cliente{
+		nome:  "Renato",
+		idade: 40,
+		ativo: true,
 	}
-	return a + b, nil
+
+	renato.cidade = "Garanhuns"
+	renato.logradouro = "av. Orlando Wanderley"
+	renato.numero = 80
+	renato.estado = "PE"
+
+	fmt.Printf(" Nome: %s\n Idade: %d\n Ativo:%t\nEndereco:\n Logradouro: %s\n N°: %d\n Cidade: %s\n Estado:%s\n\n\n\n", renato.nome, renato.idade, renato.ativo, renato.logradouro, renato.numero, renato.cidade, renato.estado)
+
+	renato.Desativar()
+
+	fmt.Printf("\n\n\n")
+
+	fmt.Printf(" Nome: %s\n Idade: %d\n Ativo:%t\nEndereco:\n Logradouro: %s\n N°: %d\n Cidade: %s\n Estado:%s\n\n\n\n", renato.nome, renato.idade, renato.ativo, renato.logradouro, renato.numero, renato.cidade, renato.estado)
+
 }
