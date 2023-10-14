ff653762c14263921a4cb061a4a4d57c23ce6d25
lealrenato <renatolealhiss@gmail.com>
2023-10-14 13:09:29 -0300

    struct composition

--- 12/main.go
+++ 12/main.go
@@ -1,22 +1,37 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 )
 
-// funçõesjhgjhgjhg
-func main() {
-	valor, err := sum(1, 50)
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println(valor)
+type Endereco struct {
+	logradouro string
+	numero     int
+	cidade     string
+	estado     string
 }
 
-func sum(a int, b int) (int, error) {
-	if a+b >= 50 {
-		return 400, errors.New("a soma é maior que 50 ")
+type Cliente struct {
+	nome  string
+	idade int
+	ativo bool
+
+	Endereco
+}
+
+// funçõesjhgjhgjhg
+func main() {
+
+	renato := Cliente{
+		nome:  "Renato",
+		idade: 40,
+		ativo: true,
 	}
-	return a + b, nil
+	renato.cidade = "Garanhuns"
+	renato.logradouro = "av. Orlando Wanderley"
+	renato.numero = 80
+	renato.estado = "PE"
+
+	fmt.Printf(" Nome: %s\n Idade: %d\n Ativo:%t\nEndereco:\n Logradouro: %s\n N°: %d\n Cidade: %s\n Estado:%s\n", renato.nome, renato.idade, renato.ativo, renato.logradouro, renato.numero, renato.cidade, renato.estado)
+
 }
