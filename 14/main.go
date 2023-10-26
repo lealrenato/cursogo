2a2adc3c06ab67d0643c6993800cc71c9fd02426
lealrenato <renatolealhiss@gmail.com>
2023-10-26 17:01:12 -0300

    interfaces

--- 14/main.go
+++ 14/main.go
@@ -1,22 +1,66 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 )
 
-// funçõesjhgjhgjhg
-func main() {
-	valor, err := sum(1, 50)
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println(valor)
+type Endereco struct {
+	logradouro string
+	numero     int
+	cidade     string
+	estado     string
 }
 
-func sum(a int, b int) (int, error) {
-	if a+b >= 50 {
-		return 400, errors.New("a soma é maior que 50 ")
+type Pessoa interface {
+	Desativar()
+}
+
+type Cliente struct {
+	nome  string
+	idade int
+	ativo bool
+	Endereco
+}
+
+type Empresa struct {
+	Nome string
+}
+
+func (e Empresa) Desativar() {
+
+}
+
+func (c Cliente) Desativar() {
+	c.ativo = false
+	fmt.Printf("cliente %s foi desativado \n", c.nome)
+	fmt.Printf("%t", c.ativo)
+}
+
+func Desativacao(pessoa Pessoa) {
+	pessoa.Desativar()
+}
+
+// funçõesjhgjhgjhg
+func main() {
+
+	renato := Cliente{
+		nome:  "Renato",
+		idade: 40,
+		ativo: true,
 	}
-	return a + b, nil
+
+	renato.cidade = "Garanhuns"
+	renato.logradouro = "av. Orlando Wanderley"
+	renato.numero = 80
+	renato.estado = "PE"
+
+	fmt.Printf(" Nome: %s\n Idade: %d\n Ativo:%t\nEndereco:\n Logradouro: %s\n N°: %d\n Cidade: %s\n Estado:%s\n\n\n\n", renato.nome, renato.idade, renato.ativo, renato.logradouro, renato.numero, renato.cidade, renato.estado)
+
+	minhaempresa := Empresa{}
+	Desativacao(minhaempresa)
+
+	fmt.Printf("\n\n\n")
+
+	fmt.Printf(" Nome: %s\n Idade: %d\n Ativo:%t\nEndereco:\n Logradouro: %s\n N°: %d\n Cidade: %s\n Estado:%s\n\n\n\n", renato.nome, renato.idade, renato.ativo, renato.logradouro, renato.numero, renato.cidade, renato.estado)
+
 }
